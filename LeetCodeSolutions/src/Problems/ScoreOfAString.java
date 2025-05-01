7779fb457dbb8655a5028f8db028cf8d1cc2b125
firstTry <oqtay.23@mail.ru>
2025-05-01 23:52:09 +0400

    stubborn commit

---
+++ LeetCodeSolutions/src/Problems/ScoreOfAString.java
@@ -0,0 +1,11 @@
+package Problems;
+
+public class ScoreOfAString {
+    public int scoreOfString(String s) {
+        int sum = 0;
+        for(int i=1; i<s.length();  i++) {
+            sum += Math.abs(s.charAt(i-1) - s.charAt(i));
+        }
+        return sum;
+    }
+}

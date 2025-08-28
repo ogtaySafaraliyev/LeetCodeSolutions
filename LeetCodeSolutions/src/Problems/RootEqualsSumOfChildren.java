120503b905d4dcd59269d39df51f300e79d7f65a
firstTry <oqtay.23@mail.ru>
2025-08-28 22:40:32 +0400

    another commit

---
+++ LeetCodeSolutions/src/Problems/RootEqualsSumOfChildren.java
@@ -0,0 +1,32 @@
+package Problems;
+
+public class RootEqualsSumOfChildren {
+	
+	public class TreeNode {
+	    int val;
+	    TreeNode left;
+	    TreeNode right;
+
+	    TreeNode() {
+	    }
+
+	    TreeNode(int val) {
+	        this.val = val;
+	    }
+
+	    TreeNode(int val, TreeNode left, TreeNode right) {
+	        this.val = val;
+	        this.left = left;
+	        this.right = right;
+	    }
+	}
+
+	class Solution {
+	    public boolean checkTree(TreeNode root) {        
+	        return root.left.val + root.right.val == root.val;
+	    }
+	}
+}
+
+
+

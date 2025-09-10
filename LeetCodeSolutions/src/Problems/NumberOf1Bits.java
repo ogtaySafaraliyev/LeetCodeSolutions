023352f428d75c46eafb21a476e860fae618c1b7
firstTry <oqtay.23@mail.ru>
2025-09-10 06:47:35 +0400

    another commit

---
+++ LeetCodeSolutions/src/Problems/NumberOf1Bits.java
@@ -0,0 +1,9 @@
+package Problems;
+
+public class NumberOf1Bits {
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);        
+    }
+
+    // check Hamming weigh with Divide and Conquer later
+}

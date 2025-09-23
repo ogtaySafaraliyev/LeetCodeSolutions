d7672482b428b3a651aee0ba49e5623d6ffa5282
firstTry <oqtay.23@mail.ru>
2025-09-23 16:55:25 +0400

    another commit

---
+++ LeetCodeSolutions/src/Problems/FactorialTrailingZeroes.java
@@ -0,0 +1,13 @@
+package Problems;
+
+public class FactorialTrailingZeroes {
+	public int trailingZeroes(int n) {
+        int ans = 0;
+
+        while(n > 0) {
+            n /= 5;
+            ans += n;
+        }
+        return ans;
+    }
+}

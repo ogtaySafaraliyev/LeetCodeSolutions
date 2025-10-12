827b34cacfccd143873a4b22aab17784e41e05d6
firstTry <oqtay.23@mail.ru>
2025-10-12 22:02:18 +0400

    another commit

---
+++ LeetCodeSolutions/src/Problems/DeleteNodeInALinkedList.java
@@ -0,0 +1,20 @@
+package Problems;
+
+public class DeleteNodeInALinkedList {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	class Solution {
+		public void deleteNode(ListNode node) {
+			node.val = node.next.val;
+			node.next = node.next.next;
+		}
+	}
+}

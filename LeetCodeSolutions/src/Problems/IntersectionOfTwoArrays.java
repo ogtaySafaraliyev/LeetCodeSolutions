14e473f25b4f1f806add61603b923cbc02300d7c
firstTry <oqtay.23@mail.ru>
2025-10-15 00:39:37 +0400

    commit after improvement

--- LeetCodeSolutions/src/Problems/IntersectionOfTwoArrays.java
+++ LeetCodeSolutions/src/Problems/IntersectionOfTwoArrays.java
@@ -2,49 +2,71 @@ package Problems;
 import java.util.*;
 
 public class IntersectionOfTwoArrays {
+//    public int[] intersection(int[] nums1, int[] nums2) {
+//        int[] count = new int[1001];
+//
+//        HashSet<Integer> set1 = new HashSet<>();
+//        HashSet<Integer> set2 = new HashSet<>();
+//        ArrayList<Integer> list = new ArrayList<>();
+//
+//        for(int num: nums1) {
+//            set1.add(num);
+//        }
+//
+//        for(int num: nums2) {
+//            set2.add(num);
+//        }
+//
+//        for(int num: set1) {
+//            list.add(num);
+//        }
+//
+//        for(int num: set2) {
+//            list.add(num);
+//        }
+//
+//
+//        List<Integer> myList = new ArrayList<>();
+//
+//        for(int num: list) {
+//            count[num]++;
+//        }
+//
+//        for(int i=0; i<count.length; i++) {
+//            if(count[i] == 2) {
+//                myList.add(i);
+//            }
+//        }
+//
+//        int[] ansArr = new int[myList.size()];
+//        int i=0;
+//
+//        for(int num: myList) {
+//            ansArr[i++] = num;
+//        }
+//        return ansArr;
+//        
+//    }
+    
     public int[] intersection(int[] nums1, int[] nums2) {
-        int[] count = new int[1001];
-
-        HashSet<Integer> set1 = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
-        ArrayList<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> finalSet = new HashSet<>();
 
         for(int num: nums1) {
-            set1.add(num);
+            set.add(num);
         }
 
         for(int num: nums2) {
-            set2.add(num);
-        }
-
-        for(int num: set1) {
-            list.add(num);
-        }
-
-        for(int num: set2) {
-            list.add(num);
-        }
-
-
-        List<Integer> myList = new ArrayList<>();
-
-        for(int num: list) {
-            count[num]++;
-        }
-
-        for(int i=0; i<count.length; i++) {
-            if(count[i] == 2) {
-                myList.add(i);
+            if(set.contains(num)) {
+                finalSet.add(num);
             }
         }
 
-        int[] ansArr = new int[myList.size()];
-        int i=0;
-
-        for(int num: myList) {
-            ansArr[i++] = num;
+        int[] arr = new int[finalSet.size()];
+        int i = 0;
+        for(int num: finalSet) {
+            arr[i++] = num;
         }
-        return ansArr;
-        
+        return arr;
     }
 }

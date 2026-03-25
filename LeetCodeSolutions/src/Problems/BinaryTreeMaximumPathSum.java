ce7666acc256c11ca7c9b91fad1459957658c3b8
firstTry <oqtay.23@mail.ru>
2026-03-26 00:01:14 +0400

    another commit

---
+++ LeetCodeSolutions/src/Problems/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,46 @@
+package Problems;
+
+public class BinaryTreeMaximumPathSum {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	class Solution {
+		int max = Integer.MIN_VALUE;
+
+		public int maxPathSum(TreeNode root) {
+			dfs(root);
+			return max;
+		}
+
+		private int dfs(TreeNode node) {
+			if (node == null)
+				return 0;
+
+			int left = Math.max(0, dfs(node.left));
+			int right = Math.max(0, dfs(node.right));
+
+			// Case: path passes through this node
+			max = Math.max(max, node.val + left + right);
+
+			// Return best single path upward
+			return node.val + Math.max(left, right);
+		}
+	}
+
+}

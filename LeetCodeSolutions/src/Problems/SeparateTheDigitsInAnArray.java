5d4ef8cdcddae1756bf4298f07f5e1c2be4a6d60
firstTry <oqtay.23@mail.ru>
2026-04-07 23:24:10 +0400

    another commit

---
+++ LeetCodeSolutions/src/Problems/SeparateTheDigitsInAnArray.java
@@ -0,0 +1,28 @@
+package Problems;
+import java.util.*;
+
+public class SeparateTheDigitsInAnArray {
+    public int[] separateDigits(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : nums) {
+            List<Integer> temp = new ArrayList<>();
+
+            while (num > 0) {
+                temp.add(num % 10); // take last digit
+                num /= 10;
+            }
+
+            Collections.reverse(temp); // fix order
+            list.addAll(temp);
+        }
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+
+}

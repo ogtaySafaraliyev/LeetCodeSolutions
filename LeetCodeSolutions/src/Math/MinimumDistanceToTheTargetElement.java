156322b81e6e6bfa64542859180bc68575abbe88
firstTry <oqtay.23@mail.ru>
2026-04-13 13:05:16 +0400

    another commit

---
+++ LeetCodeSolutions/src/Math/MinimumDistanceToTheTargetElement.java
@@ -0,0 +1,11 @@
+package Math;
+
+public class MinimumDistanceToTheTargetElement {
+    public int getMinDistance(int[] nums, int target, int start) {        
+        int minDist = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == target) minDist = Math.min(minDist, Math.abs(i - start));
+        }    
+        return minDist;
+    }
+}

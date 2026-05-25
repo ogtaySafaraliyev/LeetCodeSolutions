cf77648e4bd12c9f6c2565e4c93b60b3560b1c71
firstTry <oqtay.23@mail.ru>
2026-05-25 10:04:42 +0400

    another commit

---
+++ LeetCodeSolutions/src/problems2/FindTheDifferenceOfTwoArrays.java
@@ -0,0 +1,39 @@
+package problems2;
+import java.util.*;
+
+public class FindTheDifferenceOfTwoArrays {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        List<List<Integer>> totalList = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        for (int num : nums2) {
+            set2.add(num);
+        }
+
+        for (int num : set1) {
+            if (!set2.contains(num)) {
+                list1.add(num);
+            }
+        }
+
+        for (int num : set2) {
+            if (!set1.contains(num)) {
+                list2.add(num);
+            }
+        }
+
+        totalList.add(list1);
+        totalList.add(list2);
+
+        return totalList;
+
+    }
+}

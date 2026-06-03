b3a616a554f2f73a6a17c9aa94a19dbeccce35c5
firstTry <oqtay.23@mail.ru>
2026-06-03 23:43:16 +0400

    another commit

---
+++ LeetCodeSolutions/src/problems2/FindIndicesOfStableMountains.java
@@ -0,0 +1,12 @@
+package problems2;
+import java.util.*;
+
+public class FindIndicesOfStableMountains {
+    public List<Integer> stableMountains(int[] height, int threshold) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1; i < height.length; i++) {
+            if(height[i - 1] > threshold) list.add(i);
+        }
+        return list;
+    }
+}

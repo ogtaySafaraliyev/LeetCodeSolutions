efc02d9aaa3383b30c1150517ebcdf82beccb530
firstTry <oqtay.23@mail.ru>
2026-06-22 23:47:55 +0400

    another commit

---
+++ LeetCodeSolutions/src/problems2/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,18 @@
+package problems2;
+
+public class NumberOfStepsToReduceANumberToZero {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while(num > 0) {
+            if(num % 2 == 0) {
+                num /= 2;
+                count++;
+            }
+            else {
+                num -= 1;
+                count++;
+            }
+        }
+        return count;
+    }
+}

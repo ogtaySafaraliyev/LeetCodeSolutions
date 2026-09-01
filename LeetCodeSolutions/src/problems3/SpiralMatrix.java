8a3a38400668649d6bbdb8fac0b018c3ea503a13
firstTry <oqtay.23@mail.ru>
2026-09-01 23:52:56 +0400

    another commit

---
+++ LeetCodeSolutions/src/problems3/SpiralMatrix.java
@@ -0,0 +1,41 @@
+package problems3;
+
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+
+        int left = 0; 
+        int right = matrix[0].length - 1;
+
+        while(top <= bottom && left <= right) {
+            for(int i = left; i <= right; i++) {
+                list.add(matrix[top][i]);
+            }
+            top++;
+
+            for(int j = top; j <= bottom; j++) {
+                list.add(matrix[j][right]);
+            }
+            right--;
+            
+
+            if(top <= bottom) {
+                for(int i = right; i >= left; i--) {
+                    list.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if(left <= right) {
+                for(int j = bottom; j >= top; j--) {
+                    list.add(matrix[j][left]);
+                }
+                left++;
+            }
+        }
+        return list;        
+    }
+}
